0328fadef7218f0c1cb96d10996b8173105c801f
Pabusingh <pabs.rathore@gmail.com>
2022-08-15 16:11:08 +0530

    code update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'Pabusingh Rathore, You are genius. :D';
+  const name = process.env.NAME || 'Pabusingh Rathore';
   res.send(`Hello ${name}!`);
 });
 
